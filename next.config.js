310ade1783c7c0e6dd053f3d848027e86fba8cbd
nicco70 <nicolasgf_98@hotmail.com>
2023-10-03 01:58:12 -0300

    commit final nodesend frontend

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    backendURL: 'http://127.0.0.1:4000',
+    frontendURL: 'http://localhost:3000'
+  }
+}
+
+
+module.exports = nextConfig
---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AuthState from '../context/auth/authState';
+import AppState from '../context/app/appState';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <AuthState>
+        <AppState>
+            <Component {...pageProps} />
+        </AppState>
+    </AuthState>
+  )
+}
